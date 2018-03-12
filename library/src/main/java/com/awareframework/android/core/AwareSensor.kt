faa50e1198ca23d43c54dd070308a1c6681c3b71
Sercan Turkmen <sercanturkmen@outlook.com>
2018-03-12 11:47:24 +0200

    Broadcast receiver base for sensors
    Signed-off-by: Sercan Turkmen <sercanturkmen@outlook.com>

--- library/src/main/java/com/awareframework/android/core/AwareSensor.kt
+++ library/src/main/java/com/awareframework/android/core/AwareSensor.kt
@@ -1,7 +1,10 @@
 package com.awareframework.android.core
 
 import android.app.Service
-import android.hardware.SensorEventListener
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
 
 /**
  * Class decription
@@ -10,6 +13,14 @@ import android.hardware.SensorEventListener
  * @date 05/03/2018
  */
 
-abstract class AwareSensor : Service(), SensorEventListener {
+abstract class AwareSensor : Service() {
 
+    abstract class SensorBroadcastReceiver : BroadcastReceiver() {
+        companion object {
+            const val SENSOR_START_ENABLED = "com.aware.android.sensor.SENSOR_START"
+            const val SENSOR_STOP_ALL = "com.aware.android.sensor.SENSOR_STOP"
+        }
+
+        abstract override fun onReceive(context: Context?, intent: Intent?)
+    }
 }
