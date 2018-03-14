ef3424d0e8c2ff3618a41561f02ecb605a1ec8e3
Sercan Turkmen <sercanturkmen@outlook.com>
2018-03-14 22:59:15 +0200

    Implemented sensor sync receiver
    Signed-off-by: Sercan Turkmen <sercanturkmen@outlook.com>

--- library/src/main/java/com/awareframework/android/core/AwareSensor.kt
+++ library/src/main/java/com/awareframework/android/core/AwareSensor.kt
@@ -4,6 +4,8 @@ import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import com.awareframework.android.core.db.Engine
 
 
 /**
@@ -15,12 +17,52 @@ import android.content.Intent
 
 abstract class AwareSensor : Service() {
 
+    protected var dbEngine: Engine? = null
+    protected var sensorSyncReceiver: SensorSyncReceiver? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        sensorSyncReceiver = SensorSyncReceiver(this)
+        val syncFilter = IntentFilter()
+        syncFilter.addAction(SensorSyncReceiver.SYNC)
+        applicationContext.registerReceiver(sensorSyncReceiver, syncFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        applicationContext.unregisterReceiver(sensorSyncReceiver)
+        sensorSyncReceiver = null
+    }
+
+    fun onSync(intent: Intent?) {
+        // TODO (sercant): pass parameters directly to this function instead of passing intent
+        // TODO (sercant): parameters needs to be passed to the engine too.
+//        dbEngine?.sync()
+    }
+
     abstract class SensorBroadcastReceiver : BroadcastReceiver() {
         companion object {
             const val SENSOR_START_ENABLED = "com.aware.android.sensor.SENSOR_START"
             const val SENSOR_STOP_ALL = "com.aware.android.sensor.SENSOR_STOP"
+//            const val AWARE_SYNC = "com.aware.android.sensor.AWARE_SYNC"
         }
 
         abstract override fun onReceive(context: Context?, intent: Intent?)
+    }
+
+    class SensorSyncReceiver(val sensor: AwareSensor) : BroadcastReceiver() {
+
+        companion object {
+            const val SYNC = "com.aware.android.sensor.SYNC"
+        }
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == SYNC) {
+                // TODO (sercant): take the arguments from intent and pass them to the sensor.
+                sensor.onSync(intent)
+            }
+        }
     }
 }
